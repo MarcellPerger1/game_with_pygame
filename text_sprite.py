f66758feecb9a404a58dbc295843f167e599797a
MarcellPerger1 <marcell.perger.1@gmail.com>
2023-06-16 18:09:29 +0100

    refactor: Make `TextSprite` inherit from `RectUpdatingSprite`

--- text_sprite.py
+++ text_sprite.py
@@ -4,7 +4,7 @@ from typing import TYPE_CHECKING
 
 import pygame as pg
 
-from sprite_bases import DrawableSprite
+from sprite_bases import RectUpdatingSprite
 
 if TYPE_CHECKING:
     from main import HasGame
@@ -12,15 +12,16 @@ if TYPE_CHECKING:
 Vec2 = pg.Vector2
 
 
-class TextSprite(DrawableSprite):
+class TextSprite(RectUpdatingSprite):
     pos: Vec2 = None
     text: str = None
     always_render = False
 
     def __init__(self, game: HasGame | None, text: str, pos: Vec2 = None):
-        super().__init__(game)
+        self.set_game(game, method_name='__init__')
         self.pos = pos or self.pos
         self.set_text(text)
+        super().__init__(None)
 
     def set_text(self, text: str):
         if text != self.text or self.surf is None or self.always_render:
@@ -29,9 +30,13 @@ class TextSprite(DrawableSprite):
         if self.surf is None:
             raise RuntimeError("surface was not been returned from"
                                " render_text or wasn't set by set_surf")
+        self.update_rect()
+
+    def update_rect(self):
         self.rect = self.get_rect()
         if self.rect is None:
             raise RuntimeError("rect was not returned from get_rect")
+        self.size = self.rect.size
 
     def render_text(self) -> pg.Surface:
         raise NotImplementedError("You should override render_text when using TextSprite")
