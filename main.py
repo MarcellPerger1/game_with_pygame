c79cfc4790d939a154ceb8f886ae4551f5fab9a8
MarcellPerger1 <marcell.perger.1@gmail.com>
2023-06-17 10:22:26 +0100

    refactor: Move `EnemySpawnMgr` to new file

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import cProfile
 import logging as lg
-import random
 import sys
 import time
 from typing import Any, overload, Union, TextIO
@@ -14,6 +13,7 @@ from bullet import Bullet
 from collectable import TurretItem
 from containers import HasRect
 from enemy import CommonEnemy, EnemyWithHealth
+from enemy_spawn_mgr import EnemySpawnMgr
 from perf import PerfMgr
 from pg_util import render_text
 from sprite_bases import CommonSprite, RectUpdatingSprite
@@ -29,67 +29,10 @@ ALLOW_CHEATS = True
 FPS = 60
 SPEED = 3.8
 P_RADIUS = 20
-SPAWN_INTERVAL_START = 65
-SPAWN_ENEMY_DELAY_START = 75
-MAX_ENEMIES_PER_TICK = 18
-MIN_SPAWN_INTERVAL = 0.01
 
 
 class PGExit(BaseException):
     pass
-
-
-class EnemySpawnMgr(UsesGame):
-    def __init__(self, game: HasGame):
-        super().__init__(game)
-        self.is_enabled = False
-        self.next_enemy_time: int | None = None
-        self.enemy_spawn_interval: float = SPAWN_INTERVAL_START
-
-    def handle_enemy_spawns(self):
-        if not self.is_enabled:
-            return
-        if self.next_enemy_time is None:
-            self.next_enemy_time = self.curr_tick + SPAWN_ENEMY_DELAY_START
-        self.spawn_enemies()
-
-    def spawn_enemies(self):
-        enemies_this_tick = 0
-        while (self.next_enemy_time <= self.curr_tick
-               and enemies_this_tick < MAX_ENEMIES_PER_TICK):
-            self.spawn_enemy()
-            self.next_enemy_time += self.enemy_spawn_interval
-            enemies_this_tick += 1
-        # if it would've spawned an extra enemy, set the next enemy time to the current time
-        # (won't spawn it this frame but still allows spawning a lot next frame)
-        # should really be `ticks + epsilon` as next enemy should spawn at the very start
-        # of the next frame but we aren't spawning extra enemies this frame anyway.
-        if self.next_enemy_time < self.curr_tick:
-            self.next_enemy_time = self.curr_tick
-
-    def get_interval_decrease(self):
-        curr_val = self.enemy_spawn_interval
-        return (0.980 if curr_val > 4.0 else
-                0.990 if curr_val > 2.0 else
-                0.994 if curr_val > 0.9 else
-                0.997 if curr_val > 0.5 else
-                0.9993 if curr_val > 0.25 else
-                0.9998 if curr_val > 0.12 else
-                0.99993 if curr_val > 0.06 else
-                0.99998)
-
-    def increment_interval_once(self):
-        self.enemy_spawn_interval *= self.get_interval_decrease()
-        if self.enemy_spawn_interval < MIN_SPAWN_INTERVAL:
-            self.enemy_spawn_interval = MIN_SPAWN_INTERVAL
-
-    def spawn_enemy(self):
-        angle = random.uniform(0, 360)
-        distance = random.uniform(250, 500)
-        at = Vec2()
-        at.from_polar((distance, angle))
-        at += Vec2(self.player.pos)
-        EnemyWithHealth(self, at, 1)
 
 
 class Fonts:
