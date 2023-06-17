60602b50d7e77c3ba4e16c9dcc06403a0ec19a06
MarcellPerger1 <marcell.perger.1@gmail.com>
2023-06-17 18:22:03 +0100

    refactor: Make spawning more extensible using OPP

--- enemy_spawn_mgr.py
+++ enemy_spawn_mgr.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import math
 import random
+from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING
 
 from pygame import Vector2 as Vec2
@@ -19,8 +20,55 @@ MAX_ENEMIES_PER_TICK = 18
 MIN_SPAWN_INTERVAL = 0.01
 
 
+class EnemySpawnStrategy(UsesGame, ABC):
+    @abstractmethod
+    def spawn(self):
+        ...
+
+    @abstractmethod
+    def get_cost(self) -> float:
+        ...
+
+    def __str__(self):
+        return f'{type(self).__qualname__}'
+
+
+class SingleEnemySpawn(EnemySpawnStrategy):
+    def __init__(self, spawner: EnemySpawnMgr2, health: int = 1):
+        super().__init__(spawner)
+        self.spawner = spawner
+        self.health = health
+
+    def randomize(self):
+        """Randomizes this instance **inplace**!, returns self"""
+        target_health = math.sqrt(self.spawner.strength * 40)
+        health = random.uniform(1, target_health * 2)
+        self.game.log.debug(f"Next enemy has {health=:.2f} "
+                            f"~ uniform(1, {target_health * 2:.2f})")
+        self.health = round(health)
+        return self
+
+    @classmethod
+    def make_random(cls, spawner: EnemySpawnMgr2):
+        return cls(spawner).randomize()
+
+    def spawn(self):
+        angle = random.uniform(0, 360)
+        distance = random.uniform(250, 500)
+        at = Vec2()
+        at.from_polar((distance, angle))
+        at += Vec2(self.player.pos)
+        return EnemyWithHealth(self, at, self.health)
+
+    def get_cost(self) -> float:
+        return self.health
+
+    def __str__(self):
+        return f'{type(self).__qualname__} with health={self.health}'
+
+
 class EnemySpawnMgr2(UsesGame):
-    next_enemy_health: int
+    next_enemy: EnemySpawnStrategy
 
     def __init__(self, game: HasGame, strength=0.005, enabled=False, start_points=0.0):
         super().__init__(game)
@@ -39,11 +87,7 @@ class EnemySpawnMgr2(UsesGame):
             self.enabled = True
 
     def decide_next_enemy(self):
-        target_health = math.sqrt(self.strength*40)
-        health = random.uniform(1, target_health * 2)
-        self.game.log.debug(f"Next enemy has {health=:.2f} "
-                            f"~ uniform(1, {target_health * 2:.2f})")
-        self.next_enemy_health = round(health)
+        self.next_enemy = SingleEnemySpawn.make_random(self)
 
     def on_tick(self):
         self.check_if_should_enable()
@@ -51,24 +95,23 @@ class EnemySpawnMgr2(UsesGame):
             return
         self.points += self.strength
         self.strength += 0.0001 / 60
-        while self.points >= self.next_enemy_health:
-            self.spawn_enemy()
+        self.spawn_all()
 
     def on_kill_enemy(self, enemy: EnemyWithHealth):
         if not self.enabled:
             return
         self.strength += 0.0006 + enemy.max_hp * 0.0004
 
-    def spawn_enemy(self):
-        angle = random.uniform(0, 360)
-        distance = random.uniform(250, 500)
-        at = Vec2()
-        at.from_polar((distance, angle))
-        at += Vec2(self.player.pos)
-        health = self.next_enemy_health
-        EnemyWithHealth(self, at, health)
-        self.game.log.info(f"Spawned enemy with {health=}")
-        self.points -= health
+    def spawn_all(self):
+        while self.points >= self.next_enemy.get_cost():
+            self.spawn_from_obj(self.next_enemy)
+
+    def spawn_from_obj(self, o: EnemySpawnStrategy):
+        cost = o.get_cost()
+        assert self.points >= cost
+        o.spawn()
+        self.game.log.debug(f"Spawned {o!s}")
+        self.points -= cost
         self.decide_next_enemy()
 
 
